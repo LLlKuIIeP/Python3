3ee1e4989f87d141239ca058f46f06205d03620c
LLlKuIIeP <79032312801@yandex.ru>
2016-07-25 17:11:18 +0300

    Add files via upload

---
+++ stepic-67/module_3/lesson_4/my.py
@@ -0,0 +1,10 @@
+import re
+
+r = r"(\d*\.\d*) seconds"
+
+with open('file.txt') as file:
+    for line in file:
+	    print(5)
+	    #if re.search(r, line):
+		    #line = re.sub(r, "\1", line)
+		    #print(line)
